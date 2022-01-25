6b5ad9d5e427f3df30f349faf85f05292f818c39
christopherzh <2445058287@qq.com>
2022-01-26 00:40:01 +0800

    final

--- app/main.py
+++ app/main.py
@@ -4,6 +4,13 @@ import os
 app = FastAPI()
 
 origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
