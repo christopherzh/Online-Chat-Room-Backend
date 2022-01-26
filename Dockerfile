e32da1266e618ed4fc3ca13006b8d1a5b331150b
christopherzh <2445058287@qq.com>
2022-01-27 00:55:56 +0800

    cpmplete chat room func

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.10
 WORKDIR /app
-RUN pip install fastapi uvicorn[standard]
+RUN pip install fastapi uvicorn[standard] aioredis hiredis
 
 EXPOSE 8000
 
--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status,WebSocketDisconnect
+from typing import Optional,List
+from fastapi.responses import HTMLResponse
+import aioredis
+import hiredis
 from fastapi.middleware.cors import CORSMiddleware
 import os
+
+
 app = FastAPI()
 
 # origins = ['*']
@@ -12,19 +18,126 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
+# html = """
+# <!DOCTYPE html>
+# <html>
+#     <head>
+#         <title>Chat</title>
+#     </head>
+#     <body>
+#         <h1>WebSocket Chat</h1>
+#         <h2>Your ID: <span id="ws-id"></span></h2>
+#         <form action="" onsubmit="sendMessage(event)">
+#             <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
+#             <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+#             <button onclick="connect(event)">Connect</button>
+#             <hr>
+#             <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
+#             <button>Send</button>
+#         </form>
+#         <ul id='messages'>
+#         </ul>
+#         <script>
+#             var client_id = Date.now()
+#             document.querySelector("#ws-id").textContent = client_id;
+#             var ws = null;
+#             function connect(event) {
+#                 var itemId = document.getElementById("itemId")
+#                 var token = document.getElementById("token")
+#                 ws = new WebSocket("ws://localhost:8000/items/" + itemId.value + "/ws?token=" + token.value+"&client_id="+Number(client_id));
+#
+#                 ws.onmessage = function(event) {
+#                     var messages = document.getElementById('messages')
+#                     var message = document.createElement('li')
+#                     var content = document.createTextNode(event.data)
+#                     message.appendChild(content)
+#                     messages.appendChild(message)
+#                 };
+#                 event.preventDefault()
+#             }
+#             function sendMessage(event) {
+#                 var input = document.getElementById("messageText")
+#                 ws.send(input.value)
+#                 input.value = ''
+#                 event.preventDefault()
+#             }
+#         </script>
+#     </body>
+# </html>
+# """
 
-@app.get("/")
-async def get():
-    return {"message":"Hello!!"}
+
+# @app.get("/")
+# async def get():
+#     return {"message":"Hello!!"}
+#
+#
+# @app.websocket("/api")
+# async def chat(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Message is : {data}")
+#
+# @app.get("/test")
+# async def test():
+#     return {"message":"os.environ.get"}
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] =[]
+
+    async def connect(self,websocket:WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self,message:str,websocket:WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self,message:str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager=ConnectionManager()
+
+# @app.get("/")
+# async def get():
+#     return HTMLResponse(html)
 
 
-@app.websocket("/api")
-async def chat(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message is : {data}")
+async def get_cookie_or_token(
+        websocket: WebSocket,
+        session: Optional[str] = Cookie(None),
+        token: Optional[str] = Query(None),
+):
+    if session is None and token is None:
+        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
 
-@app.get("/test")
-async def test():
-    return {"message":"os.environ.get"}
+
+@app.websocket("/api/{item_id}/ws")
+async def websocket_endpoint(
+        websocket: WebSocket,
+        item_id: str,
+        q: Optional[int] = None,
+        cookie_or_token: str = Depends(get_cookie_or_token),
+        client_id:Optional[int]=None
+):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # await websocket.send_text(
+            #     f"Session cookie or query token value is: {cookie_or_token}"
+            # )
+            # if q is not None:
+            #     await websocket.send_text(f"Query parameter q is: {q}")
+            # await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
