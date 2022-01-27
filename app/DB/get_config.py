a85ce86cb82723df3cf6128f9fb63cd8ca5cda94
christopherzh <2445058287@qq.com>
2022-01-27 17:06:59 +0800

    add redis sub/pub

---
+++ app/DB/get_config.py
@@ -0,0 +1,11 @@
+import os
+def get_conn_url():
+    return "redis://"+get_ip()
+
+def get_ip():
+    return os.environ.get("REDIS_IP")
+def get_user():
+    return os.environ.get("REDIS_USR")
+def get_pwd():
+    return os.environ.get("REDIS_PWD")
+
