5a7a44b96bb7707cd0e3b5f7f50881bd9d17bce0
christopherzh <2445058287@qq.com>
2022-01-27 17:06:59 +0800

    add redis sub/pub

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status,WebSocketDisconnect
-from typing import Optional,List
+import asyncio
+from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status, WebSocketDisconnect
+from typing import Optional, List
+from app.DB import get_config
 from fastapi.responses import HTMLResponse
 import aioredis
 import hiredis
-from fastapi.middleware.cors import CORSMiddleware
-import os
-
 
 app = FastAPI()
 
@@ -18,53 +17,53 @@ app = FastAPI()
 #     allow_headers=["*"],
 # )
 
-# html = """
-# <!DOCTYPE html>
-# <html>
-#     <head>
-#         <title>Chat</title>
-#     </head>
-#     <body>
-#         <h1>WebSocket Chat</h1>
-#         <h2>Your ID: <span id="ws-id"></span></h2>
-#         <form action="" onsubmit="sendMessage(event)">
-#             <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
-#             <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
-#             <button onclick="connect(event)">Connect</button>
-#             <hr>
-#             <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
-#             <button>Send</button>
-#         </form>
-#         <ul id='messages'>
-#         </ul>
-#         <script>
-#             var client_id = Date.now()
-#             document.querySelector("#ws-id").textContent = client_id;
-#             var ws = null;
-#             function connect(event) {
-#                 var itemId = document.getElementById("itemId")
-#                 var token = document.getElementById("token")
-#                 ws = new WebSocket("ws://localhost:8000/items/" + itemId.value + "/ws?token=" + token.value+"&client_id="+Number(client_id));
-#
-#                 ws.onmessage = function(event) {
-#                     var messages = document.getElementById('messages')
-#                     var message = document.createElement('li')
-#                     var content = document.createTextNode(event.data)
-#                     message.appendChild(content)
-#                     messages.appendChild(message)
-#                 };
-#                 event.preventDefault()
-#             }
-#             function sendMessage(event) {
-#                 var input = document.getElementById("messageText")
-#                 ws.send(input.value)
-#                 input.value = ''
-#                 event.preventDefault()
-#             }
-#         </script>
-#     </body>
-# </html>
-# """
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
+            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+            <button onclick="connect(event)">Connect</button>
+            <hr>
+            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = null;
+            function connect(event) {
+                var itemId = document.getElementById("itemId")
+                var token = document.getElementById("token")
+                ws = new WebSocket("ws://localhost:8000/api/" + itemId.value + "/ws?token=" + token.value+"&client_id="+Number(client_id));
+
+                ws.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
+                event.preventDefault()
+            }
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
 
 # @app.get("/")
@@ -85,27 +84,38 @@ app = FastAPI()
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: List[WebSocket] =[]
+        self.active_connections: List[WebSocket] = []
+        self.redis_pool = None
+        self.pubsub = None
 
-    async def connect(self,websocket:WebSocket):
+    async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
+        if self.redis_pool is None:
+            self.redis_pool = await aioredis.from_url(
+                "redis://" + get_config.get_ip(), password=get_config.get_pwd()
+            )
+            self.pubsub = self.redis_pool.pubsub()
+        if self.pubsub is not None:
+            await self.pubsub.subscribe("channel:1")
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self,message:str,websocket:WebSocket):
+    async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
-    async def broadcast(self,message:str):
+    async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
 
-manager=ConnectionManager()
 
-# @app.get("/")
-# async def get():
-#     return HTMLResponse(html)
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
 
 
 async def get_cookie_or_token(
@@ -124,18 +134,25 @@ async def websocket_endpoint(
         item_id: str,
         q: Optional[int] = None,
         cookie_or_token: str = Depends(get_cookie_or_token),
-        client_id:Optional[int]=None
+        client_id: Optional[int] = None
 ):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
+            await manager.redis_pool.publish("channel:1", data)
+            message = await manager.pubsub.get_message(ignore_subscribe_messages=True)
+            if message is not None:
+                print(f"(Reader) Message Received: {message}")
+                await manager.send_personal_message(f"redis:{data}", websocket)
+                await manager.broadcast(f"redis for Client #{client_id} says: {data}")
             # await websocket.send_text(
             #     f"Session cookie or query token value is: {cookie_or_token}"
             # )
             # if q is not None:
             #     await websocket.send_text(f"Query parameter q is: {q}")
             # await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
+
             await manager.send_personal_message(f"You wrote: {data}", websocket)
             await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
