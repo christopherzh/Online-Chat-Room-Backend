1444a65b21c382aee82976770cdd144ddb0557a0
christopherzh <2445058287@qq.com>
2022-01-28 12:11:55 +0800

    complete redis pub/sub

--- app/main.py
+++ app/main.py
@@ -1,22 +1,11 @@
-import asyncio
-import sys
 from fastapi import Cookie, Depends, FastAPI, Query, WebSocket, status, WebSocketDisconnect
 from typing import Optional, List
 from DB import get_config
 from fastapi.responses import HTMLResponse
 import aioredis
-import hiredis
+import time
 
 app = FastAPI()
-
-# origins = ['*']
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 html = """
 <!DOCTYPE html>
@@ -44,8 +33,15 @@ html = """
             function connect(event) {
                 var itemId = document.getElementById("itemId")
                 var token = document.getElementById("token")
-                ws = new WebSocket("ws://localhost:8000/api/" + itemId.value + "/ws?token=" + token.value+"&client_id="+Number(client_id));
-
+                ws = new WebSocket("ws://localhost:8000/api/0/" + itemId.value + "/ws?token=" + token.value+"&client_id="+Number(client_id));
+                                redis = new WebSocket("ws://localhost:8000/api/1/"+Number(client_id))
+                redis.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
                 ws.onmessage = function(event) {
                     var messages = document.getElementById('messages')
                     var message = document.createElement('li')
@@ -79,26 +75,17 @@ html = """
 #         data = await websocket.receive_text()
 #         await websocket.send_text(f"Message is : {data}")
 #
-# @app.get("/test")
-# async def test():
+# @app.get("/Redis")
+# async def Redis():
 #     return {"message":"os.environ.get"}
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
-        self.redis_pool = None
-        self.pubsub = None
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
-        if self.redis_pool is None:
-            self.redis_pool = await aioredis.from_url(
-                "redis://" + get_config.get_ip(), password=get_config.get_pwd()
-            )
-            self.pubsub = self.redis_pool.pubsub()
-        if self.pubsub is not None:
-            await self.pubsub.subscribe("channel:1")
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
@@ -110,9 +97,34 @@ class ConnectionManager:
         for connection in self.active_connections:
             await connection.send_text(message)
 
+class RedisConnectionManager:
+    def __init__(self):
+        self.redis_pool = {}
+        self.pubsub = {}
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+
+    async def conn_redis(self,client_id):
+        if self.redis_pool.get(client_id) is None:
+            self.redis_pool[client_id] = await aioredis.from_url(
+                "redis://" + get_config.get_ip(), password=get_config.get_pwd()
+            )
+            self.pubsub[client_id] = self.redis_pool[client_id].pubsub()
+        if self.pubsub.get(client_id) is not None:
+            await self.pubsub[client_id].subscribe("channel:1")
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    def disconnect(self,client_id):
+        del self.pubsub[client_id]
+        del self.redis_pool[client_id]
+
+
 
 manager = ConnectionManager()
-
+redis_manager =RedisConnectionManager()
 
 @app.get("/")
 async def get():
@@ -129,7 +141,25 @@ async def get_cookie_or_token(
     return session or token
 
 
-@app.websocket("/api/{item_id}/ws")
+@app.websocket("/api/1/{client_id}")
+async def websocket_redis(websocket: WebSocket,client_id: Optional[int] = None):
+    await redis_manager.connect(websocket)
+    await redis_manager.conn_redis(client_id)
+    try:
+        while True:
+            # t1 = time.time()
+            message = await redis_manager.pubsub[client_id].get_message(ignore_subscribe_messages=True)
+            # print("time for getting msg:", time.time() - t1)
+            if message is not None and message['data'].decode('utf-8')[0:13] != str(client_id):
+                print(f"(Reader) Message Received: {message}", type(message))
+                # await manager.send_personal_message(f"redis:{message['data']}", websocket)
+                await manager.send_personal_message(
+                    f"Client #{message['data'].decode('utf-8')[0:13]} says: {message['data'].decode('utf-8')[13:]}",
+                    websocket)
+    except WebSocketDisconnect:
+        redis_manager.disconnect(client_id)
+
+@app.websocket("/api/0/{item_id}/ws")
 async def websocket_endpoint(
         websocket: WebSocket,
         item_id: str,
@@ -140,13 +170,11 @@ async def websocket_endpoint(
     await manager.connect(websocket)
     try:
         while True:
+            t2 = time.time()
             data = await websocket.receive_text()
-            await manager.redis_pool.publish("channel:1", data)
-            message = await manager.pubsub.get_message(ignore_subscribe_messages=True)
-            if message is not None:
-                print(f"(Reader) Message Received: {message}")
-                await manager.send_personal_message(f"redis:{data}", websocket)
-                await manager.broadcast(f"redis for Client #{client_id} says: {data}")
+            print("time for receive text:", time.time() - t2)
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await redis_manager.redis_pool[client_id].publish("channel:1", str(client_id) + data)
             # await websocket.send_text(
             #     f"Session cookie or query token value is: {cookie_or_token}"
             # )
@@ -154,8 +182,8 @@ async def websocket_endpoint(
             #     await websocket.send_text(f"Query parameter q is: {q}")
             # await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
 
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            # await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client #{client_id} left the chat")
