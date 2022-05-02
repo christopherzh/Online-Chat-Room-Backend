192253db8967d475817ffd7f76b5230a4e72720e
christopherzh <2445058287@qq.com>
2022-05-02 15:49:41 +0800

    [Optimize]

--- app/grpc_client.py
+++ app/grpc_client.py
@@ -11,7 +11,7 @@ from DB.redis_util import RedisController
 
 def run():
     # 连接 rpc 服务器
-    with grpc.insecure_channel('localhost:50000') as channel:
+    with grpc.insecure_channel('101.43.149.3:50051') as channel:
         # 通过通道服务一个服务
         stub = im_protobuf_pb2_grpc.WebsocketServerStub(channel)
         # 生成请求我们的服务的函数的时候，需要传递的参数体，它放在hello_pb2里面-请求体为：hello_pb2.HelloRequest对象
--- app/start.sh
+++ app/start.sh
@@ -5,7 +5,7 @@
 ## start 1
 #uvicorn im_system:app --host 0.0.0.0 --port 8000  > /var/log/im_system.log 2>&1 &
 ## start 2
-uvicorn ws_system:app --host 0.0.0.0 --port 8001 > /var/log/ws_system.log 2>&1 &
+nohup uvicorn ws_system:app --host 0.0.0.0 --port 8001 > /var/log/ws_system.log 2>&1 &
 #
 ## just keep this script running
 ## shellcheck disable=SC2160
